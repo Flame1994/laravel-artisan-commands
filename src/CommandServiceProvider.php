18242333dc20a7fc1762d00ffc3776610177cf78
Ruan <rhaarhoff@4i.co.za>
2019-05-21 15:47:44 +0200

    Removed register

--- src/CommandServiceProvider.php
+++ src/CommandServiceProvider.php
@@ -13,7 +13,7 @@ class CommandServiceProvider extends ServiceProvider
     public function register()
     {
         // used to bind any classes or functionality into the app container
-        $this->app->register('Rhaarhoff\LaravelArtisanCommands\CommandServiceProvider');
+//        $this->app->register('Rhaarhoff\LaravelArtisanCommands\CommandServiceProvider');
     }
 
     /**
